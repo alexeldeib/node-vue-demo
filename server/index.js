3f61d570ccfb626604220ef700e4f20b09712ce8
Ace Eldeib <aleldeib@microsoft.com>
2018-02-04 16:10:31 -0800

    adding server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express')
+const http = require('http');
+const https = require('https');
+var app = express();
+
+app.use(express.static(__dirname + '/../dist'));
+
+http.createServer(app).listen(80);
+https.createServer(app).listen(443);
+console.log("Listening");
